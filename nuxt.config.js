3440066ce029b1cf18b8ec476f38ebe514971156
Matt Garrett <mattga@gmail.com>
2023-06-26 09:25:39 -0700

    disable vite

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default defineNuxtConfig({
     // -- Opt-in features --
 
     // Use Vite as the bundler instead of webpack 4
-    vite: true,
+    vite: false,
 
     // Enable Nuxt 3 compatible useHead
     // meta: true,
